03727f6adfcc87fc66785ca0c408b3f1843635e7
Zakium <flakk30@gmail.com>
2015-09-13 14:19:33 +0200

    evenement

--- Espace/Assets/Moteur/EvenementGenerator.cs
+++ Espace/Assets/Moteur/EvenementGenerator.cs
@@ -7,11 +7,27 @@ namespace AssemblyCSharp
 {
 	public class EvenementGenerator
 	{
+		int entropie;							//pourcentage de chance de popage d'un event
 		public EvenementGenerator instance;
 
 		public EvenementGenerator ()
 		{
 			instance = this;
+		}
+
+		public void initialisation() 			// remettre la variable de chance d'apparition d'event a sa valeur initiale
+		{
+			entropie = 60; 						//modifiable
+		}
+
+		public void testEvent()
+		{
+			System.Random rnd = new System.Random();
+			int test = rnd.Next(0, 100);
+			if(entropie >= test)				//si oui alors faire tout les test de poids d'event sur la case et en tirer un au hasard
+			{
+			
+			}
 		}
 	}
 }
