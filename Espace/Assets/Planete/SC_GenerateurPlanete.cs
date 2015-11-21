6dc7951bd902387eac7d1bb7512de48e348d246d
Julien Nigon <julien.nigon@orange.fr>
2015-11-21 15:39:17 +0100

    Script de generation de carte debute

--- Espace/Assets/Planete/SC_GenerateurPlanete.cs
+++ Espace/Assets/Planete/SC_GenerateurPlanete.cs
@@ -17,6 +17,24 @@ public class SC_GenerateurPlanete : MonoBehaviour {
 	}
 
 	public void generer() {
+		TerrainData data = terrain.terrainData;
+		float[, ,] alphas = data.GetAlphamaps(0, 0, data.alphamapWidth, data.alphamapHeight);
 
+		for (int i = 0; i < data.alphamapWidth; i++) {
+			for (int j = 0; j < data.alphamapHeight; j++) {
+				if ((i+j)%2 == 0) {
+					alphas[i, j, 1] = 1000;
+					//alphas[i, j, 2] = 70;
+					alphas[i, j, 0] = 0;
+				}
+				else {
+					alphas[i, j, 0] = 1000;
+					alphas[i, j, 1] = 0;
+					//alphas[i, j, 2] = 70;
+
+				}
+			}
+		}
+		data.SetAlphamaps(0, 0, alphas);
 	}
 }
